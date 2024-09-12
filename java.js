6d7e939c2c550391b8fcc8b7bf8727c3b29eb1bd
eleonoradepalma <152891596+eleonoradepalma@users.noreply.github.com>
2024-09-12 11:22:18 +0200

    Update java.js

--- java.js
+++ java.js
@@ -4,3 +4,12 @@ burgerMenu.addEventListener('click',function(){
   this.classList.toggle("close");
   overlay.classList.toggle("overlay");
 });
+
+$( document ).ready(function() {
+   [].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+         img.setAttribute('src', img.getAttribute('data-src'));
+         img.onload = function() {
+            img.removeAttribute('data-src');
+         };
+   });
+});
